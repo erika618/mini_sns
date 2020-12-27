5c5d2fd08f7ea75e1dbeabad128dd5aa8d808299
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-27 22:13:41 +0900

    deviseでOumniAuthを使用できるように編集。
    Userモデルにキーの追加と、UserモデルとSnsCredentialモデルのアソシエーションを設定。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # FacebookとGoogleのOumniAuthを使用
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+  has_many :sns_credentials
 end
