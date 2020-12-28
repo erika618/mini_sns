b25363bc2f22d24606614cf0c8ccf870a72fb8e3
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-28 17:55:01 +0900

    Userモデルにクラスメソッドを作成。
    SNS認証によって得たユーザー情報は、Userモデルに入るデータのため。first_or_create、first_or_initialize、whereメソッドを駆使して、新規登録に必要なデータを検索・取得する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   has_many :sns_credentials
+
+  # omniauth_callbacks_controller.rbに記述した、User.from_omniauthを呼び出し
+  def self.from_omniauth(auth)
+    # 保存するレコードがDBに存在するか検索を行い、検索した条件のレコードがあればそのレコードを返し、なければインスタンスを保存するメソッド（保存時にUserモデルとSnsCredentialモデルを紐付け）
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+  end
 end
