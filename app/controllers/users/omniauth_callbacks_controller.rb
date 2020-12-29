caa153a8fe1d3e4546afd6d9ec3f2c5b5f0b4c28
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-29 14:45:02 +0900

    Userモデルから送られてきた情報をビューで扱えるよう実装。

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,10 +10,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def authorization
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-    if @user.persisted? # ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+    sns_info = User.from_omniauth(request.env['omniauth.auth'])
+    # @userに「nickname」と「email」の情報をもたせる
+    @user = sns_info[:user]
+    if @user.persisted?
+      # ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
       sign_in_and_redirect @user, event: :authentication
-    else # ユーザー情報が未登録なので、新規登録画面へ遷移する
+    else
+      # uidだけビューで扱えるようにする
+      @sns_id = sns_info[:sns].uid
+      # ユーザー情報が未登録なので、新規登録画面へ遷移する
       render template: 'devise/registrations/new'
     end
   end
