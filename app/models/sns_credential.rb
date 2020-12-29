377910b8030f8b6cbb984ef07d4a0f2b934975a4
erika618 <68714247+erika618@users.noreply.github.com>
2020-12-29 14:40:41 +0900

    アソシエーションにオプションを追加。
    取得したFacebookまたはGoogleの情報を、外部キーが無くても保存できるようにするため。

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,3 +1,7 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
 end
+class SnsCredential < ApplicationRecord
+  # 取得したFacebook（またはGoogle）の情報を、外部キーが無くても保存できるオプションを付与
+  belongs_to :user, optional: true
+ end
